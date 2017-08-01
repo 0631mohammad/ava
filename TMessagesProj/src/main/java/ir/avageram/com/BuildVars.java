bc6ca66a4135f030983d579fcf775091024cdd9b
0631mohammad <am.mohammad554@gmail.com>
2017-08-01 19:55:50 +0430

    add new file

---
+++ TMessagesProj/src/main/java/ir/avageram/com/BuildVars.java
@@ -0,0 +1,34 @@
+/*
+ * This is the source code of Telegram for Android v. 3.x.x.
+ * It is licensed under GNU GPL v. 2 or later.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ *
+ * Copyright Nikolai Kudashov, 2013-2017.
+ */
+
+package ir.avageram.com;
+
+public class BuildVars {
+    public static boolean DEBUG_VERSION = false;
+    public static boolean DEBUG_PRIVATE_VERSION = false;
+    public static int BUILD_VERSION = 957;
+    public static String BUILD_VERSION_STRING = "3.18";
+    public static int APP_ID = 190733; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = "82dbe772dc294817a1252c18cb4968d2"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = "82dbe772dc294817a1252c18cb4968d2";
+    public static String HOCKEY_APP_HASH_DEBUG = "82dbe772dc294817a1252c18cb4968d2";
+    public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
+    public static String FOURSQUARE_API_KEY = ""; //obtain your own KEY at https://developer.foursquare.com/
+    public static String FOURSQUARE_API_ID = ""; //obtain your own API_ID at https://developer.foursquare.com/
+    public static String FOURSQUARE_API_VERSION = "20150326";
+
+
+
+
+
+
+
+
+
+
+}
